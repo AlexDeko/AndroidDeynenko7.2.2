47add868a0d769a6756732a8e3c48e47f1068352
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-02-09 12:15:14 +0300

    myFinish

--- app/src/main/java/com/homework1_3/androiddeynenko722/MainActivity.java
+++ app/src/main/java/com/homework1_3/androiddeynenko722/MainActivity.java
@@ -10,20 +10,23 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 11;
+    private static final String LOG= "Request";
     EditText numberMobile;
     EditText txtSMS;
     Button btnSendSMS;
     Button btnCallMobile;
     String numberThisMobile;
     String txtSMSThisMobile;
-    Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,23 +43,31 @@ public class MainActivity extends AppCompatActivity {
         numberMobile =findViewById(R.id.editNumberMobile);
         btnSendSMS= findViewById(R.id.btnSendSMS);
         btnCallMobile = findViewById(R.id.btnCallMobile);
-        numberThisMobile = numberMobile.getText().toString();
-        txtSMSThisMobile = txtSMS.getText().toString();
     }
 
     private void sendSMS(){
         btnSendSMS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try {
                 if(ContextCompat.checkSelfPermission(MainActivity.this,
                         Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(MainActivity.this,
                             new String[]{Manifest.permission.SEND_SMS},
                             MY_PERMISSIONS_REQUEST_CALL_PHONE);
+                } else {
+                    String number = numberMobile.getText().toString();
+                    txtSMSThisMobile = txtSMS.getText().toString();
+                    SmsManager smgr = SmsManager.getDefault();
+                    smgr.sendTextMessage(number,null,txtSMSThisMobile,
+                            null,null);
                 }
-                SmsManager smgr = SmsManager.getDefault();
-                smgr.sendTextMessage("+79101234567",null,txtSMSThisMobile,
-                        null,null);
+                }catch (Exception e){
+                    Log.e(LOG, "Не отправляем смс");
+                    Toast.makeText(MainActivity.this, getString(R.string.toast),
+                            Toast.LENGTH_LONG).show();
+
+                }
             }
         });
 
@@ -66,16 +77,28 @@ public class MainActivity extends AppCompatActivity {
         btnCallMobile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{Manifest.permission.CALL_PHONE},
-                            MY_PERMISSIONS_REQUEST_CALL_PHONE);
+
+                try {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.CALL_PHONE},
+                                MY_PERMISSIONS_REQUEST_CALL_PHONE);
+                    } else {
+                        numberThisMobile = getString(R.string.number_placeholder,
+                                numberMobile.getText().toString() );
+                         Uri uri = Uri.parse(numberThisMobile);
+                        // Uri  uri = Uri.parse("tel:+7 (495) 152-55-28");
+                        Intent intent = new Intent(Intent.ACTION_CALL, uri);
+                        startActivity(intent);
+                    }
+
+                } catch (Exception e) {
+                    Log.e(LOG, "Не звоним");
+                    Toast.makeText(MainActivity.this, getString(R.string.toast),
+                            Toast.LENGTH_LONG).show();
+
                 }
-                // Uri  uri = Uri.parse("tel:+7 (495) 152-55-28");
-                Uri uri = Uri.parse(numberThisMobile);
-                intent = new Intent(Intent.ACTION_CALL, uri);
-                startActivity(intent);
             }
         });
     }
